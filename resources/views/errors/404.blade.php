1d1f00575fac3ae9754fe70aa6016e29c025e1bc
Angie <ajbhuige@outlook.com>
2020-05-12 17:41:28 +0200

    Edited error pages and nav bars

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,14 @@
 @extends("layout")
 @section("content")
 
-    <img src="\assets\img\404.png" alt="404" width="620" class="mx-auto d-block">
+    <img src="\assets\img\404.png" alt="404" width="580" class="mx-auto d-block error-page">
+
+    <div class="d-flex justify-content-center">
+        <a href="/">
+            <button type="button" class="btn btn-default submit">Return home
+            </button>
+        </a>
+    </div>
+
 
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,6 +1,13 @@
 @extends("layout")
 @section("content")
 
-    <img src="\assets\img\500.png" alt="500" width="620" class="mx-auto d-block">
+    <img src="\assets\img\500.png" alt="500" width="580" class="mx-auto d-block error-page">
+
+    <div class="d-flex justify-content-center">
+        <a href="/">
+            <button type="button" class="btn btn-default submit">Return home
+            </button>
+        </a>
+    </div>
 
 @endsection
