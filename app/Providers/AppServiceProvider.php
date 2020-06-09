174afa9effbdaab63f0f59475d711d75e093fe3d
Angie <ajbhuige@outlook.com>
2020-06-09 20:59:27 +0200

    https forcing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
+        Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
     }
 }
